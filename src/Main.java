f607e9a93e71fdba4bb55d0ca60b74665a7a0689
Ahlyab <ahlyabasad@gmail.com>
2023-06-01 08:18:58 +0500

    Calling stored procedure with no resultSet

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,32 @@ public class Main {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection conn = DriverManager.getConnection("jdbc:mysql:// localhost:3306/hotel_app","root","admin");
             Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from customers");
+//            ResultSet rs = stmt.executeQuery("select * from customers");
+//
+//            while(rs.next()) {
+//
+//                System.out.println(rs.getString(2));
+//            }
+            CallableStatement statement = conn.prepareCall("{CALL sp_admin_login(?,?,?)}");
+            statement.setString(1, "admin1");
+            statement.setString(2, "password1");
+            statement.registerOutParameter(3, Types.BOOLEAN);
+//            var hasResult = statement.execute();
+            var hasResult = statement.execute();
+            System.out.println(hasResult);
 
-            while(rs.next()) {
+            System.out.println(statement.getBoolean(3));
+//            if(hasResult) {
+//                System.out.println(statement.getBoolean(3));
+////                ResultSet resultSet = statement.getResultSet();
+////                System.out.println(resultSet.getFetchSize());
+////                while(resultSet.next()) {
+////                    System.out.println(resultSet.getBoolean("result"));
+////                }
+//            }else {
+//                System.out.println("Failed");
+//            }
 
-                System.out.println(rs.getString(2));
-            }
-            conn.close();
         }catch(Exception ex){
             System.out.println(ex);
         }
