000aabeedbd9af613dc62717f94902433d8dae91
AdnanArch <thedynamiccoder@gmail.com>
2023-06-30 22:25:49 +0500

    Reimplemented validator class.

--- src/main/java/security/Validator.java
+++ src/main/java/security/Validator.java
@@ -1,66 +1,83 @@
 package security;
 
-import org.jetbrains.annotations.NotNull;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-/*
- *   allow space in fname and last name
- * ` unrestrict address
- *   add . , in password(include () )
- * */
 
 public class Validator {
-    public boolean isName(@NotNull String name) {
-        for(char c : name.toCharArray()) {
-            if(c == ' ') {
-                continue;
-            }
-            if(!Character.isAlphabetic(c)){
-                return false;
-            }
+    private static final String NAME_PATTERN = "^[a-zA-Z]+$";
+    private static final String USERNAME_PATTERN = "^[a-zA-Z][a-zA-Z0-9]*$";
+    private static final String EMAIL_PATTERN = "^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-]+)(\\.[a-zA-Z]{2,5}){1,2}$";
+    private static final String PASSWORD_PATTERN = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?!.*--).{8,}$";
+    private static final String ADDRESS_PATTERN = "^[a-zA-Z0-9#]+$";
+    private static final String PHONE_PATTERN = "^\\d{11}$";
+
+    public String validateCustomerData(String firstName, String lastName, String userName, String email, String password, String address, String phone, String emergencyContact) {
+        if (isEmpty(firstName)) {
+            return "First name is required.";
+        } else if (!isFirstName(firstName)) {
+            return "First name should contain only letters.";
+        } else if (isEmpty(lastName)) {
+            return "Last name is required.";
+        } else if (!isLastName(lastName)) {
+            return "Last name should contain only letters.";
+        } else if (isEmpty(userName)) {
+            return "Username is required.";
+        } else if (!isUserName(userName)) {
+            return "Username should start with a letter and can contain letters and digits.";
+        } else if (isEmpty(email)) {
+            return "Email is required.";
+        } else if (!isEmail(email)) {
+            return "Invalid email format.";
+        } else if (isEmpty(password)) {
+            return "Password is required.";
+        } else if (!checkPassword(password)) {
+            return "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one digit.";
+        } else if (isEmpty(address)) {
+            return "Address is required.";
+        } else if (!isAddress(address)) {
+            return "Address can contain letters, numbers, and the # symbol only.";
+        } else if (isEmpty(phone)) {
+            return "Phone number is required.";
+        } else if (!isPhoneNumber(phone)) {
+            return "Invalid phone number format. It should be 03xxxxxxxxx.";
+        } else if (!isEmergencyPhoneNumber(emergencyContact)) {
+            return "Invalid emergency phone number format. It should be 03xxxxxxxxx.";
         }
-        return true;
-    }
-    public boolean isUserName(@NotNull String username) {
-        if(Character.isDigit(username.charAt(0))) {
-            return false;
-        }
-
-        for(char c : username.toCharArray()) {
-            if(!Character.isAlphabetic(c) && !Character.isDigit(c) ){
-                return false;
-            }
-        }
-        return true;
+        return null; // Data is valid
     }
 
-    public boolean isEmail(String email) {
-        return Pattern.compile("^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-]+)(\\.[a-zA-Z]{2,5}){1,2}$").matcher(email).matches();
+    private boolean isEmergencyPhoneNumber(String emergencyContact) {
+        return Pattern.matches(PHONE_PATTERN, emergencyContact);
     }
 
-    public boolean checkPassword(String password) {
-        //return Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$").matcher(password).matches();
-        return Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?!.*--).{8,}$").matcher(password).matches();
+    private boolean isEmpty(String value) {
+        return value == null || value.trim().isEmpty();
     }
 
-
-    public boolean isAddress(String addressText) {
-        return Pattern.compile("^[^-]*$").matcher(addressText).matches();
+    private boolean isFirstName(String name) {
+        return Pattern.matches(NAME_PATTERN, name);
     }
 
-    public boolean isPhoneNumber(String phoneText) {
-        return Pattern.compile("^\\d{11}$").matcher(phoneText).matches();
+    private boolean isLastName(String name) {
+        return Pattern.matches(NAME_PATTERN, name);
     }
 
-    public String errorString() {
-        String sb = "Error in provided information. kindly make sure information match the provided criteria\n\n" +
-                "1. First name and last name should only contain letters\n" +
-                "2. Username cannot start from a digit however it can end and have digit in mid\n" +
-                "3. Password : Minimum eight characters, at least one letter, one number and one special character(@$!%*#?&)\n" +
-                "4. Phone number: 03xxxxxxxxx format\n" +
-                "5. Address can contain letters, numbers and # symbol only\n" +
-                "6. Email format : example@example.com\n";
-        return sb;
+    private boolean isUserName(String username) {
+        return Pattern.matches(USERNAME_PATTERN, username);
+    }
 
+    private boolean isEmail(String email) {
+        return Pattern.matches(EMAIL_PATTERN, email);
+    }
+
+    private boolean checkPassword(String password) {
+        return Pattern.matches(PASSWORD_PATTERN, password);
+    }
+
+    private boolean isAddress(String addressText) {
+        return Pattern.matches(ADDRESS_PATTERN, addressText);
+    }
+
+    private boolean isPhoneNumber(String phoneText) {
+        return Pattern.matches(PHONE_PATTERN, phoneText);
     }
 }
