cc56c593faadb2d33d440700159e485c23d5f826
AdnanArch <thedynamiccoder@gmail.com>
2023-07-01 01:04:02 +0500

    Implemented getGeneratedOTP method.

--- src/main/java/email/OTPGenerator.java
+++ src/main/java/email/OTPGenerator.java
@@ -3,9 +3,16 @@ package email;
 import java.util.Random;
 
 public class OTPGenerator {
-    public static String generateOTP() {
+    private String generatedOTP;
+
+    public String generateOTP() {
         Random random = new Random();
         int otp = random.nextInt(900000) + 100000; // Generate a random 6-digit OTP
-        return String.valueOf(otp);
+        generatedOTP = String.valueOf(otp);
+        return generatedOTP;
+    }
+
+    public String getGeneratedOTP() {
+        return generatedOTP;
     }
 }
