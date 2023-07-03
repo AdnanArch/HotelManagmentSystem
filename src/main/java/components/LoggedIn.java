2c1324a8af61a040ee380d60b20aa420cbbba431
AdnanArch <thedynamiccoder@gmail.com>
2023-07-03 20:46:52 +0500

    Removed Unnecessary fields.

--- src/main/java/components/LoggedIn.java
+++ src/main/java/components/LoggedIn.java
@@ -6,88 +6,51 @@ import java.sql.CallableStatement;
 import java.sql.SQLException;
 import java.sql.Types;
 
-
 public class LoggedIn {
     public static String userName;
-    public static int customerID;
-    public static String customerEmail;
 
-    public static String customerName;
+    private static final DatabaseConnection db = new DatabaseConnection();
 
     public static void setUserName(String userName) {
         LoggedIn.userName = userName;
     }
 
     public static int getCustomerID() throws SQLException {
-        DatabaseConnection db = new DatabaseConnection();
-        CallableStatement callableStatement = null;
-        customerID = 0;
-
-        try {
-            String query = "{? = CALL get_customer_id(?)}";
-            callableStatement = db.connection.prepareCall(query);
+        int customerID;
+        try (CallableStatement callableStatement = db.connection.prepareCall("{? = CALL get_customer_id(?)}")) {
             callableStatement.registerOutParameter(1, Types.INTEGER);
             callableStatement.setString(2, userName);
             callableStatement.execute();
 
             customerID = callableStatement.getInt(1);
-        } finally {
-            // Close the callableStatement in the appropriate finally block
-            // to release the resources
-            if (callableStatement != null) {
-                callableStatement.close();
-            }
         }
 
         return customerID;
     }
 
     public static String getCustomerEmail() throws SQLException {
-        DatabaseConnection db = new DatabaseConnection();
-        CallableStatement callableStatement = null;
-        customerID = 0;
-
-        try {
-            String query = "{? = CALL get_user_email(?)}";
-            callableStatement = db.connection.prepareCall(query);
+        String customerEmail;
+        try (CallableStatement callableStatement = db.connection.prepareCall("{? = CALL get_user_email(?)}")) {
             callableStatement.registerOutParameter(1, Types.VARCHAR);
             callableStatement.setString(2, userName);
             callableStatement.execute();
 
             customerEmail = callableStatement.getString(1);
-        } finally {
-            // Close the callableStatement in the appropriate finally block
-            // to release the resources
-            if (callableStatement != null) {
-                callableStatement.close();
-            }
         }
 
         return customerEmail;
     }
 
     public static String getCustomerName() throws SQLException {
-        DatabaseConnection db = new DatabaseConnection();
-        CallableStatement callableStatement = null;
-        customerName = null;
-
-        try {
-            String query = "{? = CALL get_customer_name(?)}";
-            callableStatement = db.connection.prepareCall(query);
+        String customerName;
+        try (CallableStatement callableStatement = db.connection.prepareCall("{? = CALL get_customer_name(?)}")) {
             callableStatement.registerOutParameter(1, Types.VARCHAR);
             callableStatement.setString(2, userName);
             callableStatement.execute();
 
             customerName = callableStatement.getString(1);
-        } finally {
-            // Close the callableStatement in the appropriate finally block
-            // to release the resources
-            if (callableStatement != null) {
-                callableStatement.close();
-            }
         }
 
         return customerName;
     }
-
 }
